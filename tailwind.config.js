3653fa97edf20a090ed4e0377f4d5a8e24fe79f5
Daniel Still <80142739+StillScripts@users.noreply.github.com>
2022-06-28 12:16:47 +1000

    Use variable for extends classes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,27 @@
 /** @type {import('tailwindcss').Config} */
+
+const extendsConfig = {
+  colors: {
+    "wp-dark": "#1D2327",
+    "wp-middark": "#2C3338",
+    "wp-gray": "#9CA2A7",
+    "wp-blue": "#2271B1",
+    "wp-background": "#F0F0F1",
+    "wp-text": "#1d2327",
+  },
+  fontSize: {
+    "wp-sm": "13px",
+    "wp-xl": "23px",
+  },
+};
+
 module.exports = {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        "wp-dark": "#1D2327",
-        "wp-middark": "#2C3338",
-        "wp-gray": "#9CA2A7",
-        "wp-blue": "#2271B1",
-        "wp-background": "#F0F0F1",
-        "wp-text": "#1d2327"
-      },
-      fontSize: {
-        "wp-sm": "13px",
-        "wp-xl": "23px"
-      }
-    },
+    extend: extendsConfig,
   },
   plugins: [],
-}
+};
